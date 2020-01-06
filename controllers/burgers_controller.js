b3e693b83d35b0a833254682a1f8c3c8731060a2
sreeveena <sreeveena21@gmail.com>
2020-01-05 22:14:15 -0800

    working on handlebars.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,10 +2,10 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/api/burgers", function(req, res) {
+router.get("/", function(req, res) {
   burger.selectAll(function(data) {
     var burgerObject = {
       burgers: data
