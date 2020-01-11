76077170d1b6894f444ff455b3e9c5d4b14cbf0d
sreeveena <sreeveena21@gmail.com>
2020-01-10 19:27:23 -0800

    validate input

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -19,26 +19,34 @@ $(function() {
         }
       );
     });
-  
+  function validateBurgerName(name){
+    if(name == ""|| name == null || name.search(/^[0-9]*$/) != -1 || name.search(/^[ ]*$/)!= -1){
+      alert("please enter a valid Burger name.");
+      return false;
+    }
+    return true;
+  }
     $(".create-form").on("submit", function(event) {
       // Make sure to preventDefault on a submit event.
       event.preventDefault();
-  
-      var newBurger = {
-        burger_name: $("#userBurger").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/burgers", {
-        type: "POST",
-        data: newBurger
-      }).then(
-        function() {
-          console.log("created new Burger");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+      if(validateBurgerName($("#userBurger").val().trim()) == true){
+
+        var newBurger = {
+          burger_name: $("#userBurger").val().trim()
+        };
+    
+        // Send the POST request.
+        $.ajax("/api/burgers", {
+          type: "POST",
+          data: newBurger
+        }).then(
+          function() {
+            console.log("created new Burger");
+            // Reload the page to get the updated list
+            location.reload();
+          }
+        );
+      }
     });
   });
   
